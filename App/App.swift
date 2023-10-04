d9f95992c2316c7aaef579c940e784c644b0c482
Arty Fact <chris.bush+arty-fact@mongodb.com>
2023-10-04 15:06:33 +0000

    Automatic update Initiated by https://github.com/mongodb-university/realm-template-apps/commit/3c7994ac6f69939acd3c339790b9e069d1d42f72

--- App/App.swift
+++ App/App.swift
@@ -9,7 +9,7 @@ let theAppConfig = loadAppConfig()
 
 let atlasUrl = theAppConfig.atlasUrl
 
-let app = App(id: theAppConfig.appId, configuration: AppConfiguration(baseURL: theAppConfig.baseUrl, transport: nil, localAppName: nil, localAppVersion: nil))
+let app = App(id: theAppConfig.appId, configuration: AppConfiguration(baseURL: theAppConfig.baseUrl, transport: nil))
 
 @main
 struct realmSwiftUIApp: SwiftUI.App {
